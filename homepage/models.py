623393bc2245dd6547905876d87e566f9aee0280
ahfei <vgamebox@outlook.com>
2024-01-21 19:04:11 +0800

    add constraint unique

--- homepage/models.py
+++ homepage/models.py
@@ -4,7 +4,7 @@ from updatefetchWeb.settings import undefined_image_url, undefined_website, unde
 
 class Category(models.Model):
     """每个分类的一些元信息"""
-    title = models.CharField(max_length=20)
+    title = models.CharField(max_length=20, unique=True)
     description = models.CharField(max_length=200, blank=True, default='')
 
     def __str__(self):
@@ -15,7 +15,7 @@ class Category(models.Model):
 class Item(models.Model):
     """每个下载项的一些元信息"""
     category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)   # 在前层，实现未定义则划分为 未分类 的类别中，因为这里不能保证类被创建
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     image = models.URLField(blank=True, default=undefined_image_url)
     website = models.URLField(blank=True, default=undefined_website)
     version = models.CharField(max_length=50, blank=True, default=undefined_version)
@@ -30,7 +30,7 @@ class Download(models.Model):
     item = models.ForeignKey(Item, related_name='downloads', on_delete=models.CASCADE)
     platform = models.CharField(max_length=20, choices=PLATFORM_CHOICES, default="windows")
     architecture = models.CharField(max_length=20, choices=ARCHITECTURE_CHOICES, default='amd64')
-    link = models.URLField()
+    link = models.URLField(unique=True)
 
     def __str__(self):
         """返回模型的字符串表示。"""
